49cf45e368562a87f7b68965daca78d7fbda0539
Nifemi122 <oluwanifesimialasan@gmail.com>
2022-06-23 02:40:10 +0200

    first commit

---
+++ Js.js
@@ -0,0 +1,11 @@
+var firstName = "Alasan";
+var middleName = "Oluwanifesimi";
+var lastName = "Raphael";
+var height = "175cm";
+var country = "Nigeria";
+
+name = firstName + ' ' + middleName + ' ' + lastName
+
+document.write('Name:' + ' ' + name + '<br/>');
+document.write('Height:' + ' ' + height + '<br/>');
+document.write('Country:' + ' ' + country);
